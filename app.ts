e1db7d9a994067dc3098f389df46918514bdeeea
github NikolaCzv <nikola.raicic@hotmail.com>
2020-05-22 14:18:55 -0500

    Never type

--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ userInput = "Nik";
 if(typeof userInput === "string"){
     userName = userInput
 }
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code}
+}
+
+generateError("An Error Occurred!", 500)
